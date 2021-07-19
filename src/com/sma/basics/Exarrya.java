b627426c79611dacd4a410f5b5d958da7fce4f22
dude1141 <48873398+dude1141@users.noreply.github.com>
2021-07-18 22:24:20 -0400

    java
    using import to refere string from another class and using arrya

---
+++ src/com/sma/basics/Exarrya.java
@@ -0,0 +1,20 @@
+package com.sma.basics;
+
+public class Exarrya {
+
+	public static void main(String[] args) {
+		
+		
+		int[] a= {10,20,30,40,50};
+		// TODO Auto-generated method stub
+
+		
+//		int i=10;
+		for (int i=0;i<=a.length;i++){
+				
+			System.out.println("i@@@@@@"+a[i]);
+		}
+		
+	}
+
+}
