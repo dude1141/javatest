b9f69162c19293760fa58cd5ec0a0be2c954582a
dude1141 <48873398+dude1141@users.noreply.github.com>
2021-08-11 00:03:27 -0400

    abstract class
    abstract class

---
+++ src/com/sma2/javabasic/A.java
@@ -0,0 +1,8 @@
+package com.sma2.javabasic;
+
+public interface A {
+//	int x1--- this is not allowed in interface
+	public static final int x1 = 0;
+	void test1();//by  default compiler apply  "public abstract acess" modifier in interface
+
+}
